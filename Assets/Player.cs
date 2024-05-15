389eddf83671958c73a91585a8ee554a0a832bb6
Martín Calderón <elmcwl@gmail.com>
2024-05-15 16:27:53 -0700

    feat: add input script

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public float xInput;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        xInput = Input.GetAxisRaw("Horizontal");
     }
 }
